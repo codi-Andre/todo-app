8c8d3760018d663172a145f0d45faeebdc86fdbd
André Miranda <aegis.andre@gmail.com>
2025-01-06 07:52:12 -0300

    :lipstick: feat(global style): add default styles and color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 function App() {
   return (
     <>
-      <header>
+      <header className="container">
         <h1>Hello world!</h1>
       </header>
+
+      <main className="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ab eius.
+        Molestias temporibus rem ipsa aliquid non repudiandae consequuntur
+        mollitia doloribus asperiores, eveniet aperiam similique natus numquam
+        error expedita debitis.
+      </main>
     </>
   )
 }
