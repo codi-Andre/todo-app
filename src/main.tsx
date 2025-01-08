42fb7d5bfb7e56817c7442f62ef75d8318105fff
André Miranda <aegis.andre@gmail.com>
2025-01-08 20:38:35 -0300

    :recycle: refactor: reorganize app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
-import "./modern-normalize.css"
-import "./index.css"
+import "./styles/modern-normalize.css"
+import "./styles/index.css"
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
 
+import { NotFound } from "./components/not-found"
+import { DefaultCatchBoundary } from "./components/default-catch-boundary"
+
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  defaultErrorComponent: DefaultCatchBoundary,
+  defaultNotFoundComponent: NotFound,
+})
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
